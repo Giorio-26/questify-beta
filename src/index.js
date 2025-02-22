077d659fb18b6a6a8f2099cd5519d47d6d13d3b2
GeorgeGreen-1 <georgeintskorve@gmail.com>
2025-02-22 14:46:59 -0800

    Comment index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '/App';
-import './index.css'; // თუ გაქვს სტილის ფაილი
+//import './index.css'; // თუ გაქვს სტილის ფაილი
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
